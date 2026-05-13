4a34fbfa4963d6e6c7276ca3ffdc61bb77230f43
Limon ALi <limonali105@gmail.com>
2026-05-13 15:03:37 +0600

    navbar hover color issue fixed, accordion font-weight issue fixed, footer custom select added

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,6 +157,11 @@ $(document).ready(function () {
       $(".navbar").addClass("green");
     }
   });
+
+  $(".navbar-link-title").hover(function () {
+    $(this).parent().siblings().toggleClass("active");
+  });
+
   // hero-animation js end---
 
   // video review popup js start---
@@ -170,6 +175,20 @@ $(document).ready(function () {
     $(".video-review-popup").removeClass("active");
   });
   // video review popup js end---
+
+  // footer-select-filed popup js start
+  $(".footer-select-field-heading").click(function (event) {
+    event.stopPropagation();
+    $(this).parent().toggleClass("active");
+  });
+  // footer-select-filed popup js end
+
+  // filter-select-filed popup js start
+  $(".filter-select-field-heading").click(function (event) {
+    event.stopPropagation();
+    $(this).parent().toggleClass("active");
+  });
+  // filter-select-filed popup js end
 });
 // testimonial-video-play-function
 const sections = document.querySelectorAll(".testimonial-item");
@@ -230,15 +249,15 @@ const testimonialSlider = new Swiper(".testimonial-slider", {
 });
 
 // Pause on hover testimonial slider js
-const marqueeTestimonial = document.querySelector(".testimonial-slider");
+// const marqueeTestimonial = document.querySelector(".testimonial-slider");
 
-marqueeTestimonial.addEventListener("mouseenter", () => {
-  testimonialSlider.autoplay.stop();
-});
+// marqueeTestimonial.addEventListener("mouseenter", () => {
+//   testimonialSlider.autoplay.stop();
+// });
 
-marqueeTestimonial.addEventListener("mouseleave", () => {
-  testimonialSlider.autoplay.start();
-});
+// marqueeTestimonial.addEventListener("mouseleave", () => {
+//   testimonialSlider.autoplay.start();
+// });
 // testimonial-slider js end--
 
 // newsletter type js start---
@@ -256,3 +275,51 @@ newsletterField.forEach((section) => {
   });
 });
 // newsletter type js end---
+
+// filter-drop js start--
+// function DropDown(el) {
+//   this.dd = el;
+//   this.placeholder = this.dd.children(".filter-select-field-heading .title");
+//   this.opts = this.dd.find(".drop .filter-select-field");
+//   this.val = "";
+//   this.index = -1;
+//   this.initEvents();
+// }
+
+// DropDown.prototype = {
+//   initEvents: function () {
+//     var obj = this;
+//     obj.dd.on("click", function (e) {
+//       e.preventDefault();
+//       e.stopPropagation();
+//       $(this).toggleClass("active");
+//     });
+//     obj.opts
+//       .on("click", function () {
+//         var opt = $(this);
+//         obj.val = opt.text();
+//         obj.index = opt.index();
+//         obj.placeholder.text(obj.val);
+//         opt.siblings().removeClass("selected");
+//         opt.filter(':contains("' + obj.val + '")').addClass("selected");
+//       })
+//       .change();
+//   },
+//   getValue: function () {
+//     return this.val;
+//   },
+//   getIndex: function () {
+//     return this.index;
+//   },
+// };
+
+// $(function () {
+//   // create new variable for each menu
+//   var dd1 = new DropDown($("#noble-gases"));
+//   var dd2 = new DropDown($("#other-gases"));
+//   $(document).click(function () {
+//     // close menu on document click
+//     $(".wrap-drop").removeClass("active");
+//   });
+// });
+// filter-drop js end--
